24d56c476f6aa7cb64e7970f378fe378dc7975ee
Edu <eacolina@uwaterloo.ca>
2016-10-14 15:02:42 -0400

    Initial Commit

---
+++ Basic_iPhone_Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic_iPhone_Calculator
+//
+//  Created by Edu on 2016-10-14.
+//  Copyright © 2016 Whistle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
