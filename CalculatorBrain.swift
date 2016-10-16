35cdc973967e4467412c1e81959d16983a4c4032
Edu <eacolina@uwaterloo.ca>
2016-10-16 13:05:34 -0400

    Added accumulator

---
+++ CalculatorBrain.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.swift
+//  Basic_iPhone_Calculator
+//
+//  Created by Edu on 2016-10-14.
+//  Copyright © 2016 Whistle. All rights reserved.
+//
+
+import Foundation
+
+class calulatorBrain{
+    
+    var accumulator = ""
+}
