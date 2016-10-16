1df63ab83456f3d937b7ffa5bf16aea21f57eba0
Edu <eacolina@uwaterloo.ca>
2016-10-16 13:04:59 -0400

    Added touchDigit method for updating display

--- Basic_iPhone_Calculator/ViewController.swift
+++ Basic_iPhone_Calculator/ViewController.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var userIsTyping = false
+    var brain = calulatorBrain()
+    @IBOutlet weak var display: UILabel!
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        
+        let digit = sender.currentTitle!
+        if userIsTyping{
+            
+            display.text! += digit
+        } else {
+            display.text! = digit
+        }
+        
+        userIsTyping = true
+    
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
